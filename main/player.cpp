e2a7d4b9831fc91a62fd65f9bea6533538677c03
josh <github@junk.josh.com>
2020-05-28 23:41:51 -0400

    Change `if`s to `switch`, save 8 bytes

--- main/player.cpp
+++ main/player.cpp
@@ -4,15 +4,20 @@
 namespace player {
     
     Color getColor(const byte index) {
-        if(index == 0) {
-            return COLOR_PLAYER1;
+
+        switch( index ) {
+            
+            case 0 :
+                return COLOR_PLAYER1;
+            
+            case 1 : 
+                return COLOR_PLAYER2;
+            
+            case 2: 
+                return COLOR_PLAYER3;
+            
         }
-        if(index == 1) {
-            return COLOR_PLAYER2;
-        }
-        if(index == 2) {
-            return COLOR_PLAYER3;
-        }
+        
         return COLOR_PLAYER4;
     }
 }
