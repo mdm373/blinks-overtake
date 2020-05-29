284acf7910e4b33ef76ee7704a951dfeb42b6b39
josh <github@junk.josh.com>
2020-05-29 01:44:53 -0400

    Clean up some missed behavior in cached broadcasts - does not work

--- main/action.cpp
+++ main/action.cpp
@@ -2,11 +2,37 @@
 
 namespace action {
 
-    byte prev_type;
-    byte prev_payload;
+
+    bool inline send(const byte type, const byte payload , const byte face) {
+
+        if(isValueReceivedOnFaceExpired(face)) {
+            return false;
+        }
+
+        uint16_t packet = type | ( ( (uint16_t) payload ) << 8 ); 
+
+        sendDatagramOnFace( &packet , sizeof(packet), face);
+
+        return true;            
+
+    }
+
+    byte prev_broadcast_type;
+    byte prev_broadcast_payload;
+
+    inline void broadcast(const byte type, const byte payload ) {
+
+        FOREACH_FACE(f) {
+            send( type , payload , f );
+        }
+
+        prev_broadcast_type    = type;
+        prev_broadcast_payload = payload;
+        
+    }    
 
     bool isBroadcastReceived(const Action& incoming, byte type){
-        if(incoming.type == type && ( prev_type != incoming.type || prev_payload != incoming.payload)) {
+        if(incoming.type == type && ( prev_broadcast_type != incoming.type || prev_broadcast_payload != incoming.payload)) {
             broadcast(incoming.type,incoming.payload);
             return true;
         }
--- main/action.h
+++ main/action.h
@@ -13,28 +13,8 @@
 
         #define ACTION_LEN 2
 
-        bool inline send(const byte type, const byte payload , const byte face) {
-
-            if(isValueReceivedOnFaceExpired(face)) {
-                return false;
-            }
-
-            uint16_t packet = type | (payload << 8 ); 
-
-            sendDatagramOnFace( &packet , sizeof(packet), face);
-
-            return true;            
-
-        }
-
-        inline void broadcast(const byte type, const byte payload ) {
-
-            FOREACH_FACE(f) {
-                send( type , payload , f );
-            }
-            
-        }
-
+        void broadcast(const byte type, const byte payload );
+        bool send(const byte type, const byte payload , const byte face);
 
         bool isBroadcastReceived(const Action& action, const byte type);
         Action decode(const byte* buffer);
